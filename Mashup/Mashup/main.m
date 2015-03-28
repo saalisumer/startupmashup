cd441762dd188e9bc9b3b13a7d24d3e3d7c33314
saalisumer <saalisumer@yahoo.co.in>
2015-03-28 12:03:26 +0530

    init commit
    First Commit

---
+++ Mashup/Mashup/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Mashup
+//
+//  Created by Saalis Umer on 28/03/15.
+//  Copyright (c) 2015 Saalis Umer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
